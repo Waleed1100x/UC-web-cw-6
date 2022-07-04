b4783c7c277c0302e9db8e27f15fa91523512ff7
Waleed1100x <107954669+Waleed1100x@users.noreply.github.com>
2022-07-04 22:13:28 +0300

    task 1 Done

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ let search_grades_div = document.getElementById("search-grades");
 let A_grades_div = document.getElementById("A-grades");
 let bonus_grades_div = document.getElementById("bonus-grades");
 let random_grade_div = document.getElementById("random-grade");
-
-
+//4.
+grades.pop()
+//5.
+grades.push(99)
+//6.
+// grades.forEach((grade,index) => {
+//   all_grades_div.innerHTML += `<span> #${index}: ${grade} </span>`;
+// });
+//7
+for(let i = 0; i<grades.length;i++){
+  all_grades_div.innerHTML += `<span> #${i}: ${grades[i]} </span>`;
+}
 function search() {
   let inputValue = document.getElementById("input").value;
   
